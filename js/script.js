18f033a92024c5824e918ae51fd8c02bbd3cc175
Lorna Powell <powell.lap@gmail.com>
2019-11-13 15:29:50 +0000

    Challenges complete

--- js/script.js
+++ js/script.js
@@ -39,36 +39,120 @@ $(document).ready(function() {
 	    {    $("body").removeClass('page-background-color');  	}
 	);	
 
-/*
-//Make the first button hide when clicked
-	$(".html .bottom_button").click(function() {
-	$(".html .bottom_button").hide(1000);
-	}
-	);
+/*Make the first button hide when clicked
+    $("#button-effects1").click(function()
+        {   $('#button-effects1').hide('fast'); }
+    );
+*/
 
-//When the button is clicked, the paragraph above vanishes. When the button is clicked again, the paragraph reappears	
-	$(".mysql .bottom_button").click(function() {
-		$(".mysql p").toggle();
-		$(".msql p").hide(1000);
-	}
-	);
-	
-//slideToggle instead of toggle (from the last Challenge) to show or hide the paragraph		
-	$(".python .bottom_button").click(function() {
-		$(".python p").slideToggle(800);
 
+/*When the button is clicked, the paragraph above vanishes. When the button is clicked again, the paragraph reappears	
+	$("#button-effects1").click(function() {
+		$("#para1").toggle(1000);
 	}
-	);
+    );
+    	$("#button-effects2").click(function() {
+		$("#para2").toggle(1000);
+	}
+    );
+	$("#button-effects3").click(function() {
+		$("#para3").toggle(1000);
+	}
+    );
+	$("#button-effects4").click(function() {
+		$("#para4").toggle(1000);
+	}
+    );
+	$("#button-effects5").click(function() {
+		$("#para5").toggle(1000);
+	}
+    );
+	$("#button-effects6").click(function() {
+		$("#para6").toggle(1000);
+	}
+    );
+
+*/
+
+
+//slideToggle instead of toggle (as above) to show or hide the paragraph		
+	    $("#button-effects1").click(function() {
+		$("#para1").slideToggle(1000);
+	}
+    );
+    	$("#button-effects2").click(function() {
+		$("#para2").slideToggle(1000);
+	}
+    );
+	$("#button-effects3").click(function() {
+		$("#para3").slideToggle(1000);
+	}
+    );
+	$("#button-effects4").click(function() {
+		$("#para4").slideToggle(1000);
+	}
+    );
+	$("#button-effects5").click(function() {
+		$("#para5").slideToggle(1000);
+	}
+    );
+	$("#button-effects6").click(function() {
+		$("#para6").slideToggle(1000);
+	}
+    );
+
+
 
 //fadeTo in conjunction with mouseenter and mouseleave to modify the button. Fade to 50% opacity on mouseenter and fade back to 100% on mouseleave 
-	$(".jquery .bottom_button").mouseenter(function() {
+/* sample code
+$(".jquery .bottom_button").mouseenter(function() {
 	$(".jquery .bottom_button").fadeTo("slow",0.5);
 	}	)
 		$(".jquery .bottom_button").mouseleave(function() {
 	$(".jquery .bottom_button").fadeTo("slow",1);
 	}	);
-	
-	;
-	
 */	
+	$("#button-effects1").mouseenter(function() {
+	    $("#button-effects1").fadeTo("slow",0.5);
+    }	)
+        $("#button-effects1").mouseleave(function() {
+	        $("#button-effects1").fadeTo("slow",1);
+	}	);
+	$("#button-effects2").mouseenter(function() {
+	    $("#button-effects2").fadeTo("slow",0.5);
+    }	)
+        $("#button-effects2").mouseleave(function() {
+	        $("#button-effects2").fadeTo("slow",1);
+	}	);
+	$("#button-effects3").mouseenter(function() {
+	    $("#button-effects3").fadeTo("slow",0.5);
+    }	)
+        $("#button-effects3").mouseleave(function() {
+	        $("#button-effects3").fadeTo("slow",1);
+	}	);
+	$("#button-effects4").mouseenter(function() {
+	    $("#button-effects4").fadeTo("slow",0.5);
+    }	)
+        $("#button-effects4").mouseleave(function() {
+	        $("#button-effects4").fadeTo("slow",1);
+	}	);
+	$("#button-effects5").mouseenter(function() {
+	    $("#button-effects5").fadeTo("slow",0.5);
+    }	)
+        $("#button-effects5").mouseleave(function() {
+	        $("#button-effects5").fadeTo("slow",1);
+	}	);
+	$("#button-effects6").mouseenter(function() {
+	    $("#button-effects6").fadeTo("slow",0.5);
+    }	)
+        $("#button-effects6").mouseleave(function() {
+	        $("#button-effects6").fadeTo("slow",1);
+	}	);
+
+    
+
+
+
+
+
 }); 
